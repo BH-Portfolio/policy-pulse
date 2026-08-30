675a6faf65092a232ed7ba3445be7634c701f86e
Benny Hoang <benny.hoang717127@gmail.com>
2026-08-29 22:45:09 -0600

    Listed out the data params we will fetch for

--- src/ingestion/eia_client.py
+++ src/ingestion/eia_client.py
@@ -1,10 +1,28 @@
 import os
 import requests
-import pandas as pd
+import time
+from dotenv import load_dotenv
 
-class Client:
+load_dotenv()
+
+class EIAClient:
     """Class wrapping EIA API requests"""
 
-    def __init__(self):
+    def __init__(self, api_key: str):
+        self.base_url = "https://api.eia.gov/v2"
+        if not self.api_key:
+            print("No EIA API Key Found")
+        self.delay = 0.5
 
+    def get_electricity_rates(self, state: str, start: str, end: str, sector: str = "RES", frequency: str = "monthly") -> list[dict]:
+        """ Fetch retail electricity rates
+            Args:
+                state: two-letter state code
+                start: period start
+                end: period end
+                sector: "RES", "COM", "IND"
+                frequency: monthly or annual
+        """
+
+    
 
